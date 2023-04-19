679a5c30ac5f3402db2affd1b1dad9df185e5595
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2023-04-19 16:55:07 +0100

    variable setting

--- src/programs/ALL.ts
+++ src/programs/ALL.ts
@@ -2,3 +2,4 @@ export { default as help } from "./help";
 export { default as shutdown } from "./shutdown";
 export { default as clear } from "./clear";
 export { default as echo } from "./echo";
+export { default as unset } from "./unset";
---
+++ src/programs/unset.ts
@@ -0,0 +1,25 @@
+import type { Program } from "../types";
+
+export default {
+    name: "unset",
+    description: "Unset a variable.",
+    usage_suffix: "<variable>",
+    flags: {},
+    main: (data) => {
+        // extract from data to make code less verbose
+        const { args, term } = data;
+
+        // if there are no arguments, silently return
+        if (args.length === 0) {
+            return 0;
+        }
+
+        // get the variable name
+        const variable = args[0];
+
+        // delete the variable
+        term.unset_variable(variable);
+        
+        return 0;
+    }
+} as Program;
