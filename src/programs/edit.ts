f7044111824d11b1bed22dc8fc2c35644b07e071
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2023-04-25 21:52:53 +0100

    enhance fsedit command

--- src/programs/edit.ts
+++ src/programs/edit.ts
@@ -81,7 +81,7 @@ export default {
 
         // temporary note
         // TODO: remove
-        term.writeln(`${FG.yellow}Note: This program is still in development and is very broken!${NEWLINE}If you just need to read a file, use ${PREFABS.program_name}cat${STYLE.reset_all + FG.yellow}.${NEWLINE}Press any key to proceed.${STYLE.reset_all}`);
+        term.writeln(`${FG.yellow}Note: This program is still in development and is very broken!${NEWLINE}If you just need to read a file, use ${PREFABS.program_name}cat${STYLE.reset_all + FG.yellow}.${NEWLINE}If you need to edit a file, use the ${PREFABS.program_name}fsedit${STYLE.reset_all + FG.yellow} UI.${NEWLINE}Press any key to proceed.${STYLE.reset_all}`);
         await term.wait_for_keypress();
 
         // setup the screen
--- src/programs/fsedit.ts
+++ src/programs/fsedit.ts
@@ -1,19 +1,47 @@
 import type { SyncProgram } from "../types";
+import { ANSI } from "../term_ctl";
 
 export default {
     name: "fsedit",
     description: "Opens the fsedit program to edit the filesystem.",
-    usage_suffix: "",
-    arg_descriptions: {},
+    usage_suffix: "[directory]",
+    arg_descriptions: {
+        directory: "The directory to open fsedit in. Defaults to the current working directory."
+    },
     main: (data) => {
         // extract from data to make code less verbose
-        const { term } = data;
+        const { args, term } = data;
 
-        // get fs's unique name
-        const fs_name = term.get_fs().get_unique_fs_type_name();
+        // extract from ANSI to make code less verbose
+        const { PREFABS, STYLE } = ANSI;
+
+        // get fs
+        const fs = term.get_fs();
+
+        // get fs name
+        const fs_name = fs.get_unique_fs_type_name();
+
+        // check args
+        let dir = fs.get_cwd();
+        if (args.length > 1) {
+            term.writeln(`${PREFABS.error}Too many arguments.${STYLE.reset_all}`);
+            return 1;
+        } else if (args.length === 1) {
+            // set dir
+            dir = fs.absolute(args[0]);
+        }
+
+        // check if directory exists
+        if (!fs.dir_exists(dir)) {
+            term.writeln(`${PREFABS.error}Directory '${args[0]}' does not exist.${STYLE.reset_all}`);
+            return 1;
+        }
+
+        // url encode the directory
+        const encoded_dir = encodeURIComponent(dir);
 
         // open fsedit in a popup window
-        window.open(`/fsedit?type=${fs_name}`, "_blank", "popup=true")
+        window.open(`/fsedit?type=${fs_name}&dir=${encoded_dir}`, "_blank", "popup=true")
 
         // send message
         term.writeln("Opened fsedit in a new window.");
