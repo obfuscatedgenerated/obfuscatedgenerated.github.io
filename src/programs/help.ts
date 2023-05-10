c3f67dd29eb75e8a57bab5e15c38e8c8d70fbc45
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2023-05-10 22:35:13 +0100

    single column help support

--- src/programs/help.ts
+++ src/programs/help.ts
@@ -1,14 +1,29 @@
 import { ANSI, NEWLINE, ANSI_ESCAPE_REGEX } from "../term_ctl";
 import type { SyncProgram, arg_descriptions } from "../types";
 
+
+// deferred to prevent double printing of header if program has to re-execute itself
+const header = (term) => {
+    // write header
+    term.writeln(`For help on a specific command, type ${ANSI.PREFABS.program_name}help${ANSI.STYLE.reset_all} [command].`)
+    term.writeln(`The exit code of the most recently executed program is stored in the ${ANSI.PREFABS.variable_name}$?${ANSI.STYLE.reset_all} variable.`)
+    term.writeln(`You can set variables with the syntax ${ANSI.PREFABS.variable_name}variable${ANSI.STYLE.reset_all}=value and unset them with ${ANSI.PREFABS.program_name}unset${ANSI.STYLE.reset_all}.`)
+    term.writeln(`To persist the variables, define them in the ${ANSI.PREFABS.file_path}.ollierc${ANSI.STYLE.reset_all} file in your ${ANSI.PREFABS.dir_name}home${ANSI.STYLE.reset_all} directory.`)
+    term.write(NEWLINE);
+}
+
+
 export default {
     name: "help",
     description: "List programs or get help for a specific program.",
-    usage_suffix: "[command]",
+    usage_suffix: "[command | -s]",
     arg_descriptions: {
         "Arguments:": {
             "command": "The name of the program to get help for.",
-        }
+        },
+        "Flags:": {
+            "-s": "Single-column mode. Forces the program list to be displayed in a single column.",
+        },
     },
     main: (data) => {
         // extract from data to make code less verbose
@@ -16,15 +31,10 @@ export default {
 
         // extract from ANSI to make code less verbose
         const { STYLE, PREFABS } = ANSI;
-        
+
         // if no arguments are given, list all programs in 2 columns, depending on the terminal's columns
-        if (args.length === 0) {
-            // write header
-            term.writeln(`For help on a specific command, type ${PREFABS.program_name}help${STYLE.reset_all} [command].`)
-            term.writeln(`The exit code of the most recently executed program is stored in the ${PREFABS.variable_name}$?${STYLE.reset_all} variable.`)
-            term.writeln(`You can set variables with the syntax ${PREFABS.variable_name}variable${STYLE.reset_all}=value and unset them with ${PREFABS.program_name}unset${STYLE.reset_all}.`)
-            term.writeln(`To persist the variables, define them in the ${PREFABS.file_path}.ollierc${STYLE.reset_all} file in your ${PREFABS.dir_name}home${STYLE.reset_all} directory.`)
-            term.write(NEWLINE);
+        if (args.length === 0 || args[0] === "-s") {
+            const single_column = (args[0] === "-s");
 
             // get all program names
             const programs = data.registry.listPrograms();
@@ -37,36 +47,65 @@ export default {
             // sort the programs alphabetically (usually already sorted alphabetically by Object.keys, but not guaranteed)
             programs_fmt.sort();
 
-            // get the maximum length of a column
-            const max_allowable_length = Math.floor(term.cols / 2) - 1;
-            
-            // split the programs into 2 columns
-            const column1 = programs_fmt.filter((_, i) => i <= programs_fmt.length / 2);
-            const column2 = programs_fmt.filter((_, i) => i > programs_fmt.length / 2);
 
-            // pair the programs in the 2 columns
-            const paired_programs = column1.map((program1, i) => {
-                let program2 = column2[i] ?? "";
+            if (single_column) {
+                // FORMAT THE PROGRAMS INTO 1 COLUMN
 
-                const program1_real_length = program1.replace(ANSI_ESCAPE_REGEX, "").length;
-                const program2_real_length = program2.replace(ANSI_ESCAPE_REGEX, "").length;
+                header(term);
+                term.writeln(programs_fmt.join(NEWLINE));
+            } else {
+                // FORMAT THE PROGRAMS INTO 2 COLUMNS
 
-                // if the program name is too long, truncate it
-                if (program1_real_length > max_allowable_length) {
-                    program1 = program1.slice(0, max_allowable_length / 2 - 3) + "...";
+
+                // get the maximum length of a column
+                const max_allowable_length = Math.floor(term.cols / 2) - 1;
+
+
+                // split the programs into 2 columns
+                const column1 = programs_fmt.filter((_, i) => i <= programs_fmt.length / 2);
+                const column2 = programs_fmt.filter((_, i) => i > programs_fmt.length / 2);
+
+
+                // compute the length of the longest program name
+                const longest_program_length = Math.max(...programs_fmt.map((program) => program.replace(ANSI_ESCAPE_REGEX, "").length));
+
+                // compute the smallest padding length
+                const min_padding_length = max_allowable_length - longest_program_length;
+
+                // if there is negative padding (overlap, terminal too small), re-execute the program in single-column mode
+                if (min_padding_length < 0) {
+                    term.writeln("Terminal too small to display programs in 2 columns. Re-executing in single-column mode.");
+                    term.write(NEWLINE);
+                    term.execute("help -s");
+                    return 0;
                 }
-                if (program2_real_length > max_allowable_length) {
-                    program2 = program2.slice(0, max_allowable_length / 2 - 3) + "...";
-                }
 
-                // pad the programs so that they are both left-aligned
-                const padding = " ".repeat(max_allowable_length - program1_real_length);
 
-                return program1 + padding + program2;
-            });
+                // pair the programs in the 2 columns
+                const paired_programs = column1.map((program1, i) => {
+                    let program2 = column2[i] ?? "";
 
-            // write the programs to the terminal
-            term.writeln(paired_programs.join(NEWLINE));
+                    const program1_real_length = program1.replace(ANSI_ESCAPE_REGEX, "").length;
+                    const program2_real_length = program2.replace(ANSI_ESCAPE_REGEX, "").length;
+
+                    // if the program name is too long, truncate it
+                    if (program1_real_length > max_allowable_length) {
+                        program1 = program1.slice(0, max_allowable_length / 2 - 3) + "...";
+                    }
+                    if (program2_real_length > max_allowable_length) {
+                        program2 = program2.slice(0, max_allowable_length / 2 - 3) + "...";
+                    }
+
+                    // pad the programs so that they are both left-aligned
+                    const padding = " ".repeat(max_allowable_length - program1_real_length);
+                    return program1 + padding + program2;
+                });
+
+
+                // write the programs to the terminal
+                header(term);
+                term.writeln(paired_programs.join(NEWLINE));
+            }
 
             return 0;
         }
@@ -104,7 +143,7 @@ export default {
 
                 return output;
             }
-            
+
 
             term.write(NEWLINE);
             term.write(recurse(program.arg_descriptions, 0));
