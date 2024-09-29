1790ab2d6227a8648e7a0d20c21e152c56d8f149
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2024-09-29 18:30:45 +0100

    TODO

--- src/term_ctl.ts
+++ src/term_ctl.ts
@@ -202,6 +202,7 @@ export class WrappedTerminal extends Terminal {
         // TODO: single pipe to pipe output of previous command to next command
         // TODO: screen multiplexing (background programs using promises, or perhaps something cool with passing data between tabs)
         // TODO: allow certain control characters to be escaped e.g. $
+        // TODO: support sh files
 
         if (line.length === 0) {
             // if the line is empty, just move to the next line (additional check if called from external source)
