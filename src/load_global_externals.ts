48373341f99c335b17f9fb2a3b51c46b68549f22
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-06-16 18:18:50 +0100

    dont load browser specific externals on node

--- src/load_global_externals.ts
+++ src/load_global_externals.ts
@@ -1,31 +1,42 @@
 // provides expected externals of pkgbuild programs into global
 
-import * as ollieos from ".";
 import * as howler from "howler";
 import * as htmlToText from "html-to-text";
 import * as sixel from "sixel";
 import * as sweetalert2 from "sweetalert2";
 import * as xterm from "@xterm/xterm";
-import * as fitAddon from "@xterm/addon-fit";
-import * as webLinksAddon from "@xterm/addon-web-links";
-import * as imageAddon from "@xterm/addon-image";
-import * as xtermLinkProvider from "xterm-link-provider";
 
-globalThis.ollieos = ollieos;
+globalThis.ollieos = {};
 globalThis.howler = howler;
 globalThis["html-to-text"] = htmlToText;
 globalThis.sixel = sixel;
 globalThis.sweetalert2 = sweetalert2;
 globalThis["@xterm/xterm"] = xterm;
-globalThis["@xterm/addon-fit"] = fitAddon;
-globalThis["@xterm/addon-web-links"] = webLinksAddon;
-globalThis["@xterm/addon-image"] = imageAddon;
-globalThis["xterm-link-provider"] = xtermLinkProvider;
 
 // support old xterm names
 globalThis["xterm"] = xterm;
-globalThis["xterm-addon-fit"] = fitAddon;
-globalThis["xterm-addon-web-links"] = webLinksAddon;
-globalThis["xterm-addon-image"] = imageAddon;
 
-console.log("Global externals loaded successfully.");
+
+console.log("Common global externals loaded successfully.");
+
+if (typeof window !== "undefined") {
+    console.log("Loading browser-specific global externals...");
+
+    (async() => {
+        const fitAddon = await import("@xterm/addon-fit");
+        const webLinksAddon = await import("@xterm/addon-web-links");
+        const imageAddon = await import("@xterm/addon-image");
+        const xtermLinkProvider = await import("xterm-link-provider");
+
+        globalThis["@xterm/addon-fit"] = fitAddon;
+        globalThis["@xterm/addon-web-links"] = webLinksAddon;
+        globalThis["@xterm/addon-image"] = imageAddon;
+        globalThis["xterm-link-provider"] = xtermLinkProvider;
+
+        globalThis["xterm-addon-fit"] = fitAddon;
+        globalThis["xterm-addon-web-links"] = webLinksAddon;
+        globalThis["xterm-addon-image"] = imageAddon;
+
+        console.log("Browser-specific global externals loaded successfully.");
+    })();
+}
