24e7b7585487e6ca834d233c97463b58b375c4e4
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-01-10 02:53:07 +0000

    descriptions of program fields

--- src/types.ts
+++ src/types.ts
@@ -16,17 +16,34 @@ export interface ProgramMainData {
 
 export type ProgramMain = (data: ProgramMainData) => Promise<number>;
 
-export type arg_descriptions = { [key: string]: string | arg_descriptions }; // any level of nested key:string pairs. each key is a section title, until the innermost object, in which they are pairs of argument name and description.
+export type ArgDescriptions = { [key: string]: string | ArgDescriptions }; // any level of nested key:string pairs. each key is a section title, until the innermost object, in which they are pairs of argument name and description.
 export interface Program {
+    // command to execute the program, should ideally match the filename for consistency
     name: string,
+
+    // a short description of the program, shown in help listings
     description: string,
+
+    // suffix to show in usage, e.g. "file [options]"
     usage_suffix: string,
-    arg_descriptions: arg_descriptions,
-    node_opt_out?: boolean, // default false, if true it will not be registered if running in node
-    hide_from_help?: boolean, // default false, if true it will be hidden from the program listing in help, but can still be found by typing help <program>. useful for services
+
+    // descriptions of each argument the program takes, shown in help <program>
+    arg_descriptions: ArgDescriptions,
+
+    // default false, if true it will not be registered if running in node
+    node_opt_out?: boolean,
+
+    // default false, if true it will be hidden from the program listing in help, but can still be found by typing help <program>. useful for services and triggers
+    hide_from_help?: boolean,
+
+    // async entry point of the program, returning the exit code
     main: ProgramMain,
+
+    // optional tab completion generator
     completion?: CompletionGenerator,
-    compat?: string // should be set to "2.0.0" for programs that have been ported to the 2.0.0+ API
+
+    // should be set to "2.0.0" for programs that have been ported to the 2.0.0+ API. if not set, considered to be "1.x" legacy program that will not run.
+    compat?: string
 }
 
 // TODO: move some of these to their correct modules
