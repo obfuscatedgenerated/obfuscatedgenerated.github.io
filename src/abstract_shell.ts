095b66bad603a2fdffea88312134557ff28ae83a
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-01-11 20:45:22 +0000

    remove old shell func

--- src/abstract_shell.ts
+++ src/abstract_shell.ts
@@ -36,6 +36,4 @@ export abstract class AbstractShell {
 
     // edit_doc_title should default to true in implementations
     abstract execute(line: string, edit_doc_title?: boolean, program_final_completion_callback?: (exit_code?: number) => void): Promise<boolean>;
-
-    abstract handle_privilege_request?(reason: string, process: UserspaceOtherProcessContext): Promise<boolean>;
 }
