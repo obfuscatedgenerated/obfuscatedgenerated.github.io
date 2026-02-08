fbcdd44eaf115f4973374a56ef5c8acd15d3ae35
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-02-08 21:28:55 +0000

    Update abstract_shell.ts

--- src/abstract_shell.ts
+++ src/abstract_shell.ts
@@ -36,4 +36,6 @@ export abstract class AbstractShell {
 
     // edit_doc_title should default to true in implementations
     abstract execute(line: string, edit_doc_title?: boolean, program_final_completion_callback?: (exit_code?: number) => void): Promise<boolean>;
+
+    // TODO: expose parse method
 }
