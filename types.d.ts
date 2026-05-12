07d23d4bb63f077461b89d3380420543d3f0e8a3
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-05-12 22:38:09 +0000

    Deploying to types from @ obfuscatedgenerated/obfuscatedgenerated.github.io@a6b83703963410b8bca95a0c5338f4b786016e99 🚀

--- types.d.ts
+++ types.d.ts
@@ -40,6 +40,7 @@ export type ProgramMain<K = UserspaceKernel> = (data: ProgramMainData<K>) => Pro
 export type PrivilegedProgramMain = ProgramMain<Kernel>;
 /**
  * Properties related to how the program should be displayed in 3rd party GUI listings, such as start menus, search, etc.
+ * @group Program Types
  * @category Programs
  */
 export interface ProgramGUIProps {
